80efcb5d7b4cd5d7b27ddc1cabc9c556c19e3a99
Ubuntu <ubuntu@ip-172-31-4-42.ap-south-1.compute.internal>
2024-12-30 09:37:58 +0000

    c

---
+++ script.groovy
@@ -0,0 +1,7 @@
+//add line
+//constants.groovy
+import groovy.transform.field
+field string gitURL= "https://github.com/NirupSingh2377/shared-library.git"
+field string password= "Nirup@12345"
+field string artifactory ID "cf03c01a-a8c1-48f0-b40b-5a9bd4ae3232"
+
