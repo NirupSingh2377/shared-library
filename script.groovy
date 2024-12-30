8abcfba7b501fdaaa24dee8c6e294983e4c22e2e
NirupSingh2377 <nirupsinghsagar@gmail.com>
2024-12-30 15:16:15 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,7 +1,6 @@
-//add line
-//constants.groovy
-import groovy.transform.field
-field string gitURL= "https://github.com/NirupSingh2377/shared-library.git"
-field string password= "Nirup@12345"
-field string artifactory ID "cf03c01a-a8c1-48f0-b40b-5a9bd4ae3232"
+// constants.groovy
+import groovy.transform.Field
 
+@Field String gitURL = "https://github.com/NirupSingh2377/shared-library.git"
+@Field String password = "Nirup@12345"
+@Field String artifactoryID = "cf03c01a-a8c1-48f0-b40b-5a9bd4ae3232"
